fc6222b6c71eea6df63176a11bf4e165565f10ff
Pablo Miranda Carranza <pablo.miranda@arch.kth.se>
2014-11-10 17:16:31 +0100

    Updated project names and readme

--- WebsocketServo/WebsocketServo.ino
+++ WebsocketServo/WebsocketServo.ino
@@ -1,14 +1,13 @@
-#include <SPI.h>
-#include <Ethernet.h>
-#include <cmath>
-#include <Servo.h>
-//#include "ServoData.h"
 
 // Enabe debug tracing to Serial port.
 #define DEBUG
 // Here we define a maximum framelength to 64 bytes. Default is 256.
 #define MAX_FRAME_LENGTH 64
 
+#include <SPI.h>
+#include <Ethernet.h>
+//#include <cmath>
+#include <Servo.h>
 #include <WebSocket.h>
 
 //This is the mac address. It needs to be different for every connected device
@@ -17,10 +16,12 @@ byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x0E, 0x61 };
 //in case DHCP does not work, or we want to hardcode the IP
 byte ip[] = {130, 237, 11,243};
 
-// Create a Websocket server
+//Websocket server
 WebSocket wsServer;
 
-ServoManager servos;
+//Servo stuff
+Servo servo;
+const int ServoPin=7;
 
 //These are the websocket functions
 void sendMessage(WebSocket &socket, char* message){
@@ -56,52 +57,23 @@ void onData(WebSocket &socket, char* dataString, byte frameLength) {
   
   int pos=0;
   
-  for(int i=0;i< frameLength || i <3;++i){
+  for(int i=0;i< frameLength && i <3;++i){
     //in ASCII digits are characters with values between
     //48 (0) and 57 (9). Thus if the input is not between those 
     //values, it is not a number.
-    if(dataString[i]<48 || data[pos]>57){
+    if(dataString[i]<48 || dataString[i]>57){
       sendMessage(socket,"INCORRECT MESSAGE");
       return; 
     }
     
-    
-    
+    //convert ASCII to decimal digit and shift
+    pos=pos*10+dataString[i]-48; 
   }
   
-
-//
-//  //we process the data:
-//  dataString[frameLength]='\0'; //It is not properly formatted!!
-//  ServoData sdata(dataString);
-//
-//  if(!sdata.isValid()){
-//    sendMessage(socket,"INCORRECT MESSAGE");
-//    return;
-//  }
-//  char message[100];
-//  switch(sdata.getCommand()){
-//      case 'a': //attach
-//        servos.attach(sdata.getIndex());
-//        sprintf(message,"%d attached.",sdata.getIndex());
-//        break;
-//      case 'd':
-//        servos.detach(sdata.getIndex());
-//        sprintf(message,"%d detached.",sdata.getIndex());
-//      case 'm':
-//        servos.setDestination(sdata.getIndex(),sdata.getVal());
-//        sprintf(message,"%d move to %d",sdata.getIndex(),sdata.getVal());
-//        break;
-//      case 's':
-//        servos.setSpeed(sdata.getIndex(),sdata.getVal());
-//        sprintf(message,"%d speed to %d",sdata.getIndex(),sdata.getVal());
-//        break;
-//      default:
-//        sprintf(message,"ERROR IN PROCESSING COMMAND");
-//  }
-
-  sendMessage(socket,message);
-
+  char message[30];
+  sprintf(message,"moving servo to position %d.",pos);
+  servo.write(pos);
+  
 #ifdef DEBUG
   Serial.println(message);
 #endif
@@ -109,19 +81,20 @@ void onData(WebSocket &socket, char* dataString, byte frameLength) {
 }
 
 
+//THIS IS THE ARDUINO SETUP 
 void setup() {
 #ifdef DEBUG  
   Serial.begin(57600);
 #endif
   //Ethernet.begin(mac, ip);
 
-// start the Ethernet connection:
-if (Ethernet.begin(mac) == 0) {
-  Serial.println("Failed to configure Ethernet using DHCP");
-  // try to congifure using IP address instead of DHCP:
-  Ethernet.begin(mac, ip);
-}
-
+  // start the Ethernet connection:
+  
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
 
 
 #ifdef DEBUG  
@@ -135,11 +108,17 @@ if (Ethernet.begin(mac) == 0) {
   wsServer.begin();
   
   delay(100); // Give Ethernet time to get ready
+  
+  //initialise servo 
+ // servo.attach(ServoPin); //this takes the default max and min positions
+  servo.attach(ServoPin, 544, 2300); //this is adjusted to the parallax servos
+  
 }
+
+//ARDUINO LOOP
 
 void loop(){
   // Should be called for each loop.
   wsServer.listen();
-  //this is the way we do if they have speed...
-  servos.update();
+ 
 }
